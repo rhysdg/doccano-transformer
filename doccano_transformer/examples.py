3bee8090b2c1160841da82516de9b53bb5255986
Rhys Williams <rhysdgwilliams@gmail.com>
2022-07-07 22:45:55 +0100

    quick fix

--- doccano_transformer/examples.py
+++ doccano_transformer/examples.py
@@ -34,7 +34,7 @@ class NERExample:
             return labels
         elif 'label' in self.raw:
             labels = defaultdict(list)
-            for label in self.raw['labels']:
+            for label in self.raw['label']:
                 # TODO: This format doesn't have a user field currently.
                 # So this method uses the user 0 for all label.
                 labels[0].append(label)
